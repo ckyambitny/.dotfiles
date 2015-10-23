2c0d8b470eb4e9728215576fdcbdef91420eb634
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-10-23 23:13:47 +0200

    omg i forgot to commit

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ cp -v .gitignore ~/.gitignore
 cp -v .aliases ~/.aliases
 cp -v .bashrc ~/.bashrc
 cp -v .gitconfig ~/.gitconfig
-cp -v .vimrc ~/.vimrc 
+cp -v .Xmodmap ~/.Xmodmap
+cp -v .vimrc ~/.vimrc
 vim +PluginInstall +qall
 source ~/.bashrc
